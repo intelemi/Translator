65c1db334bf0a9351b7af84e941ed43c392af3fa
BlusterG05 <jerycohe05@gmail.com>
2024-10-23 01:19:44 -0500

    Dinamic seleccion prompt/assintant content

--- gemini_model/model.py
+++ gemini_model/model.py
@@ -1,5 +1,6 @@
 import google.generativeai as genai
-import gemini_model.prompts as prompts
+from google.generativeai import caching
+from prompts import prompts
 from dotenv import load_dotenv
 import os
 
@@ -8,15 +9,16 @@ load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 MODEL_NAME = os.getenv("MODEL_NAME")
 
+
 genai.configure(api_key=GEMINI_API_KEY)
 
 class GeminiInteract:
-    def __init__(self, prompt_key='profesional', 
-                 temperature=1, top_p=0.95, 
+    def __init__(self, prompt_key='query_no_translation_tsafiqui', 
+                 temperature=0.3, top_p=0.95, 
                  top_k=40, 
                  max_output_tokens=8192):
         self.__model = MODEL_NAME
-        self.__system_prompt = self.load_prompt(prompt_key)
+        self.__prompt_config = prompts[prompt_key]  # Acceso directo al diccionario de prompts
         self.__generation_config = {
             "temperature": temperature,
             "top_p": top_p,
@@ -26,18 +28,27 @@ class GeminiInteract:
         }
         self.chat_session = None
 
-    def load_prompt(self, prompt_key):
-        return prompts.prompts.get(prompt_key, "Default system instruction")
-
     def start_chat(self):
         if not self.chat_session:
+            # Crear el modelo con la configuración del sistema
             model = genai.GenerativeModel(
                 model_name=self.__model,
                 generation_config=self.__generation_config,
-                system_instruction=self.__system_prompt,
+                system_instruction=self.__prompt_config['system']
             )
-            print(self.__system_prompt)
-            self.chat_session = model.start_chat()
+
+            # Iniciar el chat con el diccionario como primer mensaje del modelo
+            initial_history = []
+            if self.__prompt_config.get('assistant'):
+                initial_history = [
+                    {
+                        "role": "model",
+                        "parts": [str(self.__prompt_config['assistant'])]
+                    }
+                ]
+
+            self.chat_session = model.start_chat(history=initial_history)
+            print(f"System Prompt loaded: {self.__prompt_config['system'][:50]}...")
 
     def send_single_message(self, message):
         self.start_chat() 
@@ -46,9 +57,10 @@ class GeminiInteract:
             return response
         except Exception as e:
             return f"Error: {str(e)}"
+        
 
 
 if __name__ == "__main__":
-    gemini = GeminiInteract(prompt_key='creativo')
+    gemini = GeminiInteract(prompt_key='query_translation_tsafiqui_to_spanish')
     response = gemini.send_single_message(message='¿Qué puedes hacer por mí?')
     print(response.text)
